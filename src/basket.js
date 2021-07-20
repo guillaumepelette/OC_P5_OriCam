1ce3512513bfcb91921d8f2397d271baa3af80a0
cleanbitmaker <guillaume.pelette@protonmail.com>
2021-07-20 21:59:09 +0200

    fixed issues with modifying basket product quantities and added option to delete products

--- src/basket.js
+++ src/basket.js
@@ -6,7 +6,8 @@ function loadBasket() {
     if (!Array.isArray(basket)) {
         basket = [];
         document.querySelector('.totalQuantity').textContent = 0;
-    } else {
+    }
+    else {
         let totalQuantity = 0;
         for (let i = 0; i < basket.length; i++) {
             totalQuantity += Number(basket[i].quantity);
@@ -17,7 +18,21 @@ function loadBasket() {
     return basket;
 }
 
+function checkIfBasketIsEmpty() {
+    let basket = loadBasket();
+    if (basket.length == 0) {
+        const emptyBasketMessageDivElement = document.getElementById('basket-products-container');
+        const emptyBasketMessageParagraph = document.createElement('p');
+        emptyBasketMessageParagraph.textContent = "Votre panier est actuellement vide.";
+        emptyBasketMessageParagraph.id = "emptyBasketMessageParagraph";
+        emptyBasketMessageDivElement.appendChild(emptyBasketMessageParagraph);
+        let deliveryCosts = document.querySelector('h6.mb-3');
+        deliveryCosts.textContent = "Livraison: ";
+    }
+}
+
 document.onload = loadBasket();
+checkIfBasketIsEmpty();
 
 // On récupère les données depuis l'API avec la méthode GET
 const urlOrinoco = 'http://localhost:3000/api/cameras/';
@@ -33,12 +48,6 @@ var myInit = {
     credentials: "same-origin",
     cache: 'default'
 };
-
-// 1. Load basket
-// 2. forEach basketProduct, get + add missingProductData
-// 3. Return finalBasket
-// 4. Show product cards on basket page
-
 
 async function getMissingProductData() {
     const response = await fetch(urlOrinoco , myInit);
@@ -102,7 +111,7 @@ async function createProductCard() {
         const cameraCards = document.getElementById('basket-products-container');
         
         const productCard = document.createElement('div')
-        productCard.classList = "card";
+        productCard.classList.add("card", "productCard");
         productCard.style.display = "flex";
         productCard.style.flexFlow = "row nowrap";
         productCard.style.maxWidth = "100%";
@@ -146,20 +155,19 @@ async function createProductCard() {
         const lens = document.createElement('p');
         lens.textContent = ` - Lentille :  ${finalBasket[i].lens}`;
         lens.style.marginTop = "0.2rem";
+        lens.style.marginBottom = "0px";
         let regexRule = /[^\w\d\\s]/gi;
         let modifiedLens = finalBasket[i].lens.replace(regexRule, "");
         
         const price = document.createElement('p');
         price.textContent = `Prix :  ${finalBasket[i].price/100} €`;
-        price.style.marginTop = "0.5rem";
+        price.style.marginTop = "0.2rem";
         price.style.textAlign = "left";
         price.style.marginBottom = "0px";
         
         const quantity = document.createElement('span');
+        quantity.id = 'quantityText';
         quantity.textContent = "Quantité: ";
-        quantity.style.marginTop = "0.5rem";
-        quantity.style.textAlign = "left";
-        quantity.style.marginBottom = "0px";
 
         const quantityNumber = document.createElement('span');
         quantityNumber.id = 'quantityNumber'+ i.toString();
@@ -180,20 +188,26 @@ async function createProductCard() {
         iconsDiv.style.display = "flex";
         iconsDiv.style.flexFlow = "row nowrap";
         
-        const plusIcon = document.createElement('span')
-        plusIcon.classList.add(finalBasket[i].cameraId, modifiedLens);
-        plusIcon.id = 'plusIcon';
-        plusIcon.innerHTML = '<i class="fas fa-plus"></i>';
-        
         const minusIcon = document.createElement('span')
         minusIcon.classList.add(finalBasket[i].cameraId, modifiedLens);
         minusIcon.id = 'minusIcon';
         minusIcon.innerHTML = '<i class="fas fa-minus"></i>';
         minusIcon.style.marginRight = "0.5rem";
-        
+
+        const plusIcon = document.createElement('span')
+        plusIcon.classList.add(finalBasket[i].cameraId, modifiedLens);
+        plusIcon.id = 'plusIcon';
+        plusIcon.innerHTML = '<i class="fas fa-plus"></i>';
+        plusIcon.style.marginRight = "0.5rem";
+
+        const trashIcon = document.createElement('span');
+        trashIcon.classList.add(finalBasket[i].cameraId, modifiedLens);
+        trashIcon.id = 'trashIcon';
+        trashIcon.innerHTML = '<i class="fas fa-trash-alt"></i>';
+
         const div = document.createElement('div');
         div.id = 'cameraDiv';
-        
+
         const link = document.createElement('a');
         link.class="link"
         link.id = 'linkId';
@@ -215,22 +229,34 @@ async function createProductCard() {
         div.appendChild(iconsDiv);
         iconsDiv.appendChild(minusIcon);
         iconsDiv.appendChild(plusIcon);
+        iconsDiv.appendChild(trashIcon);
     }
 
     computeBasketValue(totalQuantityValue, totalQuantity, finalSubTotalValue, finalSubTotal, finalTotalValue);
 
-    let quantityIcons = document.querySelectorAll('span>i.fas');
-    console.log(quantityIcons);
+    let productModificationIcons = document.querySelectorAll('span>i.fas');
+    console.log(productModificationIcons);
 
-    let minusIcon = document.querySelectorAll('#minusIcon');
-    let plusIcon = document.querySelectorAll('#plusIcon');
+    let minusIcons = document.querySelectorAll('#minusIcon');
+    let plusIcons = document.querySelectorAll('#plusIcon');
+    let trashIcons = document.querySelectorAll('#trashIcon');
 
-    quantityIcons.forEach(icon => {
+    productModificationIcons.forEach(icon => {
         icon.style.pointerEvents = "none";
+        let hoverableSpan = icon.parentElement;
+        console.log(hoverableSpan);
+        hoverableSpan.addEventListener('mouseenter', function(event) {
+            let span = event.target
+            span.style.cursor = 'pointer';
+        })
+        hoverableSpan.addEventListener('mouseleave', function(event) {
+            let span = event.target
+            span.style.cursor = 'auto';
+        })
     });
 
-    for (let i=0; i < minusIcon.length; i++) {
-        minusIcon[i].addEventListener("click", function(event) {
+    for (let i=0; i < minusIcons.length; i++) {
+        minusIcons[i].addEventListener("click", function(event) {
             let targetElement = event.target;
             console.log(targetElement);
             elementRelatedProduct = targetElement.classList.value.split(" ");
@@ -238,16 +264,34 @@ async function createProductCard() {
             let indexProductInBasket = finalBasket.findIndex(
                 (product => product._id === elementRelatedProduct[0]) &&
                 (product => product.uniqueLensId === elementRelatedProduct[1]));
-            let basketProductAdded = decreaseBasketQuantity(indexProductInBasket, finalBasket, totalQuantity, finalSubTotal);
-            console.log((basketProductAdded.price)/100);
-            totalQuantity -=1;
-            finalSubTotal -= (basketProductAdded.price)/100;
-            computeBasketValue(totalQuantityValue, totalQuantity, finalSubTotalValue, finalSubTotal, finalTotalValue);
+            console.log(finalBasket[indexProductInBasket]);
+            console.log(finalBasket[indexProductInBasket].quantity);
+            console.log(typeof finalBasket[indexProductInBasket].quantity);
+            if (finalBasket[indexProductInBasket].quantity == 1) {
+                let deleteConfirmation = window.confirm("Vous ne pouvez pas réduire la quantité de cet article à 0.\nEtes-vous sûr(e) de vouloir supprimer cet article?");
+                if (deleteConfirmation) {
+                    console.log(finalBasket[indexProductInBasket].quantity);
+                    totalQuantity -= finalBasket[indexProductInBasket].quantity;
+                    finalSubTotal -= finalBasket[indexProductInBasket].quantity*((finalBasket[indexProductInBasket].price)/100);
+                    console.log(finalBasket[indexProductInBasket]);
+                    console.log(finalBasket[indexProductInBasket].quantity);
+                    console.log((finalBasket[indexProductInBasket].price)/100);
+                    deleteProductFromBasket(indexProductInBasket, finalBasket, totalQuantity, finalSubTotal);
+                    computeBasketValue(totalQuantityValue, totalQuantity, finalSubTotalValue, finalSubTotal, finalTotalValue);
+                    location.reload();
+                }
+            } else {
+                let basketProductRemoved = decreaseBasketQuantity(indexProductInBasket, finalBasket);
+                console.log((basketProductRemoved.price)/100);
+                totalQuantity -=1;
+                finalSubTotal -= (basketProductRemoved.price)/100;
+                computeBasketValue(totalQuantityValue, totalQuantity, finalSubTotalValue, finalSubTotal, finalTotalValue);
+            }
         });
     }
 
-    for (let i=0; i < plusIcon.length; i++) {
-        plusIcon[i].addEventListener("click", function(event) {
+    for (let i=0; i < plusIcons.length; i++) {
+        plusIcons[i].addEventListener("click", function(event) {
             let targetElement = event.target;
             console.log(targetElement);
             elementRelatedProduct = targetElement.classList.value.split(" ");
@@ -257,9 +301,33 @@ async function createProductCard() {
                 (product => product.uniqueLensId === elementRelatedProduct[1]));
             let basketProductAdded = increaseBasketQuantity(indexProductInBasket, finalBasket);
             console.log((basketProductAdded.price)/100);
-            totalQuantity +=1;
+            totalQuantity += 1;
             finalSubTotal += (basketProductAdded.price)/100;
             computeBasketValue(totalQuantityValue, totalQuantity, finalSubTotalValue, finalSubTotal, finalTotalValue);
+        });
+    }
+
+    for (let i=0; i < trashIcons.length; i++) {
+        trashIcons[i].addEventListener("click", function(event) {
+            let targetElement = event.target;
+            console.log(targetElement);
+            elementRelatedProduct = targetElement.classList.value.split(" ");
+            console.log(elementRelatedProduct);
+            let indexProductInBasket = finalBasket.findIndex(
+                (product => product._id === elementRelatedProduct[0]) &&
+                (product => product.uniqueLensId === elementRelatedProduct[1]));
+            let deleteConfirmation = window.confirm("Etes-vous sûr(e) de vouloir supprimer cet article?");
+            if (deleteConfirmation) {
+                console.log(finalBasket[indexProductInBasket].quantity);
+                totalQuantity -= finalBasket[indexProductInBasket].quantity;
+                finalSubTotal -= finalBasket[indexProductInBasket].quantity*((finalBasket[indexProductInBasket].price)/100);
+                console.log(finalBasket[indexProductInBasket]);
+                console.log(finalBasket[indexProductInBasket].quantity);
+                console.log((finalBasket[indexProductInBasket].price)/100);
+                deleteProductFromBasket(indexProductInBasket, finalBasket, totalQuantity, finalSubTotal);
+                computeBasketValue(totalQuantityValue, totalQuantity, finalSubTotalValue, finalSubTotal, finalTotalValue);
+                location.reload();
+            }
         });
     }
 }
@@ -276,6 +344,7 @@ function decreaseBasketQuantity(index, basket) {
     basket[index].quantity -= 1;
 
     let quantityElement = document.getElementById('quantityNumber'+index.toString());
+    console.log(quantityElement);
     quantityElement.textContent = basket[index].quantity;
 
     let stringBasket = JSON.stringify(basket);
@@ -287,12 +356,25 @@ function decreaseBasketQuantity(index, basket) {
 
 function increaseBasketQuantity(index, basket) {
     console.log(basket[index].quantity);
+    basket[index].quantity = Number(basket[index].quantity);
+    console.log(typeof basket[index].quantity);
     basket[index].quantity += 1;
     console.log(basket[index].quantity);
     
     let quantityElement = document.getElementById('quantityNumber'+index.toString());
     quantityElement.textContent = basket[index].quantity;
     
+    let stringBasket = JSON.stringify(basket);
+    localStorage.setItem('basket', stringBasket);
+    loadBasket();
+    console.log(basket[index]);
+    return basket[index];
+}
+
+function deleteProductFromBasket(index, basket) {
+    console.log(basket[index].quantity);
+    basket.splice(index, 1);
+
     let stringBasket = JSON.stringify(basket);
     localStorage.setItem('basket', stringBasket);
     loadBasket();
